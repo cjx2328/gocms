07216536a6f464bedff053b5f5673251b4db90f0
kurtchen <cjx2328@gmail.com>
2020-12-29 17:54:24 +0800

    delect email config js

--- website/manageweb/src/api/app/sys/emailconfig.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function requestemailconfig(token){
-  return request({
-    url: '/emailconfig/list',
-    method: 'get',
-    param: { token }
-  })
-}
